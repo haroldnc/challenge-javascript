ab55ad1cb30159dfc0773841af39cb6d1aa7fe8a
haroldnc <harold.mth95@gmail.com>
2022-07-02 13:04:16 -0500

    Ejercicio 9 terminado

--- checkpoint.js
+++ checkpoint.js
@@ -299,7 +299,14 @@ BinarySearchTree.prototype.toArray = function() {
 // informarse sobre algoritmos, leerlos de un pseudocodigo e implemnterlos alcanzara
 
 function primalityTest(n) {
+    if (n === 2 || n === 3) return true;
+    if (n <= 1 || n % 2 == 0 || n % 3 == 0) return false;
+
+    for (let i = 5; i * i <= n ; i+=6) {
+        if (n % i == 0 || n % (i + 2) == 0) return false;
+    }
     
+    return true;
 }
 
 
